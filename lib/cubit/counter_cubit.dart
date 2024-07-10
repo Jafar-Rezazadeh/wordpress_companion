5483a3349258b45744a1846dc4a6cbe0ec8e009b
Jafar-Rezazadeh <jafarrezazadeh76@gmail.com>
2024-07-10 19:12:27 +0330

    cleaning up to start

--- lib/cubit/counter_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
-
-  increase() => emit(state + 1);
-
-  decrease() => emit(state - 1);
-}
