8a17f63ce7c9460b5272dba381311c074676b3f1
jafar <jafarrezazadeh76@gmil.com>
2024-11-10 13:44:56 +0330

    little test modification for custom workflow

--- test/core/entities/profile_avatar_test.dart
+++ test/core/entities/profile_avatar_test.dart
@@ -2,20 +2,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wordpress_companion/core/entities/profile_avatar.dart';
 
 void main() {
-  test("should contain the expected values in the props ", () {
-    //arrange
-    const profileAvatar = ProfileAvatar(
-      size24px: "url1",
-      size48px: "url2",
-      size96px: "url3",
-    );
+  // test("should contain the expected values in the props ", () {
+  //   //arrange
+  //   const profileAvatar = ProfileAvatar(
+  //     size24px: "url1",
+  //     size48px: "url2",
+  //     size96px: "url3",
+  //   );
 
-    //act
-    final result = profileAvatar.props;
+  //   //act
+  //   final result = profileAvatar.props;
 
-    //assert
-    expect(result, containsAll(["url1", "url2", "url3"]));
-  });
+  //   //assert
+  //   expect(result, containsAll(["url1", "url2", "url3"]));
+  // });
 
   test("should be same if the object has the same field values ", () {
     //arrange
